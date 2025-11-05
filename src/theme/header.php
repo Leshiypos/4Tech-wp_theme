97a0e11bcd008aabd1a55f70df749b7039079d92
Wp_devsite <leshiy_pos@mail.ru>
2025-11-05 17:00:22 +0300

    Starting equipment Template page

--- src/theme/header.php
+++ src/theme/header.php
@@ -15,42 +15,12 @@
   <head>
     <meta charset="<?php bloginfo( 'charset' ); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <!-- <link rel="stylesheet" href="./assets/libs/swiper/swiper-bundle.min.css" /> -->
-    <!-- <link rel="stylesheet" href="./assets/fonts/stylesheet.css" /> -->
-    <!-- <link rel="stylesheet" href="./assets/css/styles.css" /> -->
-    <!-- <link
-      rel="stylesheet"
-      href="https://unpkg.com/lenis@1.3.4/dist/lenis.css"
-    /> -->
-
-    <!-- <script src="https://cdn.jsdelivr.net/npm/gsap@3.13.0/dist/gsap.min.js"></script> -->
-    <!-- <script src="https://cdn.jsdelivr.net/npm/gsap@3.12.5/dist/ScrollTrigger.min.js"></script> -->
-    <!-- <script src="./assets/libs/swiper/swiper-bundle.min.js"></script> -->
-    <!-- <script src="https://unpkg.com/lenis@1.3.4/dist/lenis.min.js"></script> -->
-    <!-- <script src="./assets/js/main.js"></script> -->
-    <!-- <script src="./assets/js/sliders.js"></script> -->
-
-    <!-- <script
-      type="module"
-      src="https://unpkg.com/@google/model-viewer/dist/model-viewer.min.js"
-    ></script> -->
-    <!-- Проверка номера -->
-    <!-- CSS -->
-    <!-- <link
-      rel="stylesheet"
-      href="https://cdn.jsdelivr.net/npm/intl-tel-input@23.7.4/build/css/intlTelInput.css"
-    /> -->
-
-    <!-- JS -->
-    <!-- <script src="https://cdn.jsdelivr.net/npm/intl-tel-input@23.7.4/build/js/intlTelInput.min.js"></script>
-    Нужен для форматирования/валидации -->
-
 
 	<?php wp_head(); ?>
   </head>
   <body <?php body_class(); ?>>
 	<?php wp_body_open(); ?>
-    <div id="preloader">
+    <!-- <div id="preloader">
       <div class="wrap_preloader">
         <div class="wrap_img back">
           <img src="<?php echo get_template_directory_uri(); ?>/assets/images/logo.svg" alt="" />
@@ -59,5 +29,5 @@
           <img src="<?php echo get_template_directory_uri(); ?>/assets/images/logo.svg" alt="" />
         </div>
       </div>
-    </div>
+    </div> -->
 
